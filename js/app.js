c59d4f6b726d77773ea1e596e0fa8f07cfe97ef4
samiah1 <s.a.alhoraibi@gmail.com>
2019-03-06 22:50:13 +0300

    A description of what was added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
 /*
   Please add all Javascript code to this file.
 */
+
+$(function () {
+
+    $.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=8da72323d5d14430bb987b68ae63e07f",
+        function (res) {
+            console.log(res.articles);
+
+            res.articles.forEach(ele => {
+                console.log(ele.title);
+                console.log(ele.url);
+                $("#main").find(".article").append(`
+           
+           <section class="featuredImage">
+           <img src="${ele.urlToImage}" alt="" />
+           </section>
+           <section class="articleContent">
+            <a href="${ele.url}"><h3>${ele.title}</h3></a>
+            <h6>${ele.description}</h6>
+           </section>
+
+       <section class="impressions">
+         526
+       </section>
+
+       <div class="clearfix"></div>
+       
+           `)
+
+            })
+
+        });
+    $('.loader').hide();
+});
+
